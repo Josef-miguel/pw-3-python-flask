0d81bbe7e28ef411a9a2a07bb3b24b3527df29ef
miguel <migsjs77@gmail.com>
2025-03-18 10:35:45 -0300

    dia de hoje

--- aula-02-arquitetura-mvc-request/controllers/routex.py
+++ aula-02-arquitetura-mvc-request/controllers/routex.py
@@ -1,24 +1,34 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 jogadores = ['Miguel José', 'Miguel Isack', 'Leaf', 'Quemario', 'Trop', 'aspax', 'maxxdiego']
+
+ #Dicionário em python (Objeto em python)
+        #Array de objetos [{},{},{}]
+gamelist = [{
+           'titulo' : 'CS-GO',
+                'ano' : 2012,
+        'categoria' :'FPS Online'
+            
+        }]
+consolelist = [{
+    'nome' : '',
+    'fabricante' : '',
+    'ano' : '',
+    'preco': ''
+}]
 def init_app(app):
         #Criando a primeira rota do site
     @app.route('/')
     #Criando função no python
     def home():
         return render_template('index.html')
-    # Rota de games {do lado esquerdo é chave do lado direito}
+    # Rota de games {do lado esquerdo é chave do lado direito os valores}
     @app.route('/games', methods=['GET','POST'])
     def games(): 
-        #Dicionário em python (Objeto em python)
-        game = {
-            'titulo' : 'CS-GO',
-                'ano' : 2012,
-        'categoria' :'FPS Online'
-            
-        }
+      
+        game = gamelist[0]
+  
 
 
-        categoria = 'FPS Online'
         
         #Tratando se a requisição for do tipo POST
         if request.method == 'POST':
@@ -35,3 +45,22 @@ def init_app(app):
                                 game=game,
                             jogadores=jogadores,
                             jogos=jogos)
+    #Rota de cadastro de jogos (EM dicionario)
+    @app.route('/cadgames', methods=['GET','POST'])
+    def cadgames():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'titulo': request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria': request.form.get('categoria')})
+            return redirect(url_for('cadgames'))
+        return render_template('cadgames.html',
+                               gamelist=gamelist)
+        
+        #Rota de cadastro de consoles
+    @app.route('/consoles', methods=['GET','POST'])
+    def console():
+        if request.method == 'POST':
+            if request.form.get('nome') and request.form.get('fabricante') and request.form.get('ano') and request.form.get('preco'):
+                consolelist.append({'nome': request.form.get('nome'), 'fabricante' : request.form.get('fabricante'), 'ano': request.form.get('ano'), 'preco' : request.form.get('preco')})
+            return redirect(url_for('console'))
+        return render_template('consoles.html',
+                               consolelist=consolelist)
