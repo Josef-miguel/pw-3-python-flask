fc1cfbf056f45d406dfe6582a1b3b8db4c965894
Josef-miguel <migsjs77@gmail.com>
2025-09-02 11:27:03 -0300

    aula do dia 02/09

--- aula-07-relacionamento-entre-tabelas/models/database.py
+++ aula-07-relacionamento-entre-tabelas/models/database.py
@@ -28,9 +28,10 @@ class Game(db.Model):
     #Criando o relacionamento
     console = db.relationship('Console', backref=db.backref('games',lazy=True))
 
-    def __init__(self, titulo, ano, categoria, preco, quantidade):
+    def __init__(self, titulo, ano, categoria, preco, quantidade, console_id):
         self.titulo = titulo
         self.ano = ano
         self.categoria = categoria
         self.preco = preco
-        self.quantidade = quantidade
+        self.quantidade = quantidade 
+        self.console_id = console_id
